ed02ecd8e5902453c3199b550a764f3eda44d192
Kaleidosium <iamrifki0@gmail.com>
2022-10-24 12:11:03 +0700

    Addendum Commit - Fix Controls

--- source/modules/player.nim
+++ source/modules/player.nim
@@ -5,14 +5,15 @@ import modules/shooter
 
 proc controlsGame*(playerShip: var PlayerShip; centerNumber: var CenterNumber;
     modifierSlots: var ModifierSlots; gameOver: bool) =
-  if not screenStopOn or not gameOver:
-    if keyIsDown(kiLeft):
-      playerShip.angle += 350
-    if keyIsDown(kiRight):
-      playerShip.angle -= 350
-    if keyHit(kiA):
-      let bulPlayerProj = initProjectileBulletPlayer(gfxBulletTemp,
-          playerShip.body.pos)
-      shooter.fireBulletPlayer(bulPlayerProj, playerShip.angle)
-    if keyHit(kiB):
-      centerNumber.inputModifierValue(modifierSlots)
+  if not gameOver:
+    if not screenStopOn:
+      if keyIsDown(kiLeft):
+        playerShip.angle += 350
+      if keyIsDown(kiRight):
+        playerShip.angle -= 350
+      if keyHit(kiA):
+        let bulPlayerProj = initProjectileBulletPlayer(gfxBulletTemp,
+            playerShip.body.pos)
+        shooter.fireBulletPlayer(bulPlayerProj, playerShip.angle)
+      if keyHit(kiB):
+        centerNumber.inputModifierValue(modifierSlots)
